317970ac576979afb03ecc9af0029297e0a94563
stigmat <56791300+stigmatdev@users.noreply.github.com>
2021-10-10 14:36:22 +0300

    test production

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href="/favicon-{{ App::environment() }}.ico">
+    <link rel="shortcut icon" href="/favicon-local.ico">
     <link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/style.css">
